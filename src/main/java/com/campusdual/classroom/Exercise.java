d82d8f7ea1da6c951b93eb1c119cf63df4147013
Miriam Carballo Rodríguez <miriamcarballorodriguez@gmail.com>
2025-11-06 16:46:46 +0100

    Solved Exercise 31

--- src/main/java/com/campusdual/classroom/Exercise.java
+++ src/main/java/com/campusdual/classroom/Exercise.java
@@ -2,14 +2,25 @@ package com.campusdual.classroom;
 
 import com.campusdual.Utils;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+
 public class Exercise {
 
     public static void main(String[] args) {
+        String input = generateStringToSave(null);
+        printToFile(input);
 
     }
 
     public static String generateStringToSave(String string) {
-
+        if (string != null) {
+            return string;
+        }
+        return generateUserInputToSave();
     }
 
     private static String generateUserInputToSave(){
@@ -21,10 +32,13 @@ public class Exercise {
         }
         return sb.toString();
     }
-
     public static void printToFile(String string) {
-
+        Path path = Paths.get("src/main/resources/data.txt");
+        try{
+            Files.createDirectories(path.getParent());
+            Files.write(path, string.getBytes(), StandardOpenOption.CREATE, StandardOpenOption.TRUNCATE_EXISTING);
+        } catch (IOException e) {
+            throw new RuntimeException("Error al escribie en fichero", e);
+        }
     }
-
-
 }
